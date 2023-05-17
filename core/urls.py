9e4c7c607bb657c87397adb253f0d157a7108a6a
Leonardo de Paula <leonardopx@gmail.com>
2023-05-17 01:00:15 -0300

    Bootstrap

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 # Views
-from core.views import *
+from core.views import IndexView, ContinuousModeView
 
 app_name = 'core'
 
@@ -10,5 +10,6 @@ urlpatterns = [
     
     # Rotas
     path('',  IndexView.as_view(), name='index'),
+    path('continuous/',  ContinuousModeView.as_view(), name='continuous-mode'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,20 @@
 # Django
 from django.views.generic import TemplateView
 
+# Combinational Motifs
+from core.models import Motif
+
 class IndexView(TemplateView):
 
-    template_name = "core/index.html"
+    template_name = 'core/index.html'
+
+class ContinuousModeView(TemplateView):
+
+    template_name = 'core/continuous.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ContinuousModeView, self).get_context_data(**kwargs)
+
+        context['motifs'] = Motif.objects.all()
+
+        return context
