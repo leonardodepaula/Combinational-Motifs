7cd4e16c1fae4ed9756a472d2453f77ce2ada3d0
Leonardo de Paula <leonardopx@gmail.com>
2023-05-17 12:10:02 -0300

    Next

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -11,8 +11,8 @@ def get_diagrams(motif):
     return Diagram.objects.filter(motif=motif)
 
 @register.simple_tag
-def get_result(diagram, user):
+def get_result(diagram_pk, user_pk):
     try:
-        return Resolution.objects.filter(diagram=diagram, user=user).last().success
+        return Resolution.objects.filter(diagram__pk=diagram_pk, user__pk=user_pk).last().success
     except:
         return None
