443093e27dc6fb2512894ad2beacd0de7e89221b
Leonardo de Paula <leonardopx@gmail.com>
2023-05-17 11:27:13 -0300

    color buttons

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -2,10 +2,17 @@
 from django import template
 
 # Combinational Motifs
-from core.models import Diagram
+from core.models import Diagram, Resolution
 
 register = template.Library()
 
 @register.simple_tag
 def get_diagrams(motif):
     return Diagram.objects.filter(motif=motif)
+
+@register.simple_tag
+def get_result(diagram, user):
+    try:
+        return Resolution.objects.filter(diagram=diagram, user=user).last().success
+    except:
+        return None
